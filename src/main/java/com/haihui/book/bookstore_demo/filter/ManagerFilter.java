f19572c96a9181c1ad0d615b8820586a7f70a059
lhh <lihaihuifs@outlook.com>
2022-05-08 11:45:48 +0800

    User Module: Filter, prevent not logged in clients from accessing to manager resources

---
+++ src/main/java/com/haihui/book/bookstore_demo/filter/ManagerFilter.java
@@ -0,0 +1,33 @@
+package com.haihui.book.bookstore_demo.filter;
+/**
+ * @author : lihai
+ * @mailto : lihaihuifs@outlook.com
+ * @created : 2022/5/8, 星期日
+ * @version : 1.0
+ **/
+
+import com.haihui.book.bookstore_demo.entity.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @description  :
+ * @version      : [v1.0]
+ */
+public class ManagerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        User user = (User) httpServletRequest.getSession().getAttribute("user");
+
+        if (user == null){
+            // Not logged in: go to log in page
+            request.getRequestDispatcher("/pages/user/login.jsp").forward(request,response);
+        } else {
+            // Already log in, let go
+            chain.doFilter(request,response);
+        }
+    }
+}
